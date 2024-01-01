2d136d5aee4ddaac01519e577593f96d7b65d6d1
zoewang27 <zmwang27@gmail.com>
2024-01-01 18:35:21 +0100

    make the cart quantity interactive

--- script/amazon.js
+++ script/amazon.js
@@ -77,6 +77,13 @@ document.querySelectorAll('.js-add-to-cart') //对所有的button进行操作
       quantity: 1
       })
    }
-    console.log(cart)
+   let cartQuantity = 0;
+
+   cart.forEach((item)=>{
+      cartQuantity += item.quantity;
+   })
+
+   document.querySelector('.js-cart-quantity')
+   .innerHTML = cartQuantity;
   })
 })
