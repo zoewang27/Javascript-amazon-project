ab14825a733fb4dddcd62743b11b92a820701be0
zoewang27 <zmwang27@gmail.com>
2024-01-01 18:29:54 +0100

    make add to cart button interactive

--- script/amazon.js
+++ script/amazon.js
@@ -13,7 +13,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-rating-container">
-            <img class="product-rating-stars" src="images/ratings/rating-${product.rating.star * 10}">
+            <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
@@ -45,7 +45,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart
+          " data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -54,3 +55,28 @@ products.forEach((product) => {
 
 document.querySelector('.products-grid')
 .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart') //对所有的button进行操作
+.forEach((button) => {
+  button.addEventListener('click',()=>{ //加监听
+   const productId = button.dataset.productId; //获取ID；可以通过属性data-product-id的product-id获取到productID
+
+   let matchingItem; 
+
+   cart.forEach((item)=>{ //循环对比cart里面的productId
+    if (productId === item.productId) { 
+      matchingItem = item; //找到后赋值给matchingItem
+    }
+   })
+
+   if (matchingItem){ //如果matchingItem不为空，即在cart中存在
+    matchingItem.quantity += 1; //则数量+1
+   } else{ //否则，则将该产品加入到cart中
+    cart.push({
+      productId: productId,
+      quantity: 1
+      })
+   }
+    console.log(cart)
+  })
+})
