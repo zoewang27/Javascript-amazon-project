fbc286f266d2de8b2055b95f7e452e60e0e2003c
zoewang27 <zmwang27@gmail.com>
2024-01-02 09:22:27 +0100

    regenerate HTML when updating delivery option

--- script/checkout.js
+++ script/checkout.js
@@ -4,86 +4,30 @@ import {formatCurrency} from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions } from '../data/deliveryOptions.js'; 
 
+function renderOrderSummary(){
+  let cartSunmaryHTML ='';
 
-let cartSunmaryHTML ='';
+  cart.forEach((cartItem)=>{
+    const productId = cartItem.productId;
+    let matchingProduct;
 
-cart.forEach((cartItem)=>{
-  const productId = cartItem.productId;
-  let matchingProduct;
+    //利用productId去products找出该产品的所有信息
+    products.forEach((product)=>{ 
+      if (product.id === productId) { 
+        matchingProduct = product; 
+      }
+    })
+  
+    const deliveryOptionId = cartItem.deliveryOptionId;
+    //利用deliveryOptionId去找出deliveryOption对象的该日期
+    let deliveryOption;
+    deliveryOptions.forEach((option)=>{
+      if(option.id === deliveryOptionId){
+        deliveryOption = option; 
+      }
+    })
 
-  //利用productId去products找出该产品的所有信息
-  products.forEach((product)=>{ 
-    if (product.id === productId) { 
-      matchingProduct = product; 
-    }
-   })
- 
-   const deliveryOptionId = cartItem.deliveryOptionId;
-   //利用deliveryOptionId去找出deliveryOption对象的该日期
-   let deliveryOption;
-   deliveryOptions.forEach((option)=>{
-    if(option.id === deliveryOptionId){
-      deliveryOption = option; 
-    }
-   })
-
-   //转化为需要用的日期格式
-   const today = dayjs();
-   const deliveryDate = today.add(
-     deliveryOption.deliveryDays, 
-     'days'
-   );
-   const dateString = deliveryDate.format(
-     'dddd, MMMM D'
-   );
-
-
-  cartSunmaryHTML +=
-  `
-  <div class="cart-item-container 
-    js-cart-item-container-${matchingProduct.id}">
-    <div class="delivery-date">
-      Delivery date: ${dateString}
-    </div>
-
-    <div class="cart-item-details-grid">
-      <img class="product-image" src="${matchingProduct.image}">
-
-      <div class="cart-item-details">
-        <div class="product-name">
-          ${matchingProduct.name}
-        </div>
-        <div class="product-price">
-          $${formatCurrency(matchingProduct.priceCents)}
-        </div>
-        <div class="product-quantity">
-          <span>
-            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-          </span>
-          <span class="update-quantity-link link-primary">
-            Update
-          </span>
-          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-            Delete
-          </span>
-        </div>
-      </div>
-
-      <div class="delivery-options">
-        <div class="delivery-options-title">
-          Choose a delivery option:
-        </div>
-        ${deliveryOptionsHTML(matchingProduct,cartItem)}
-      </div>
-    </div>
-  </div>
-  `
-})
-
-
-function deliveryOptionsHTML(matchingProduct,cartItem){
-  let html = '';
-  deliveryOptions.forEach((deliveryOption)=>{
+    //转化为需要用的日期格式
     const today = dayjs();
     const deliveryDate = today.add(
       deliveryOption.deliveryDays, 
@@ -93,60 +37,120 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
       'dddd, MMMM D'
     );
 
-    const priceString = deliveryOption.priceCents 
-    === 0 
-    ? 'Free'
-    :  `$${formatCurrency(deliveryOption.priceCents)} -`;
 
-    const isChecked = deliveryOption.id === 
-    cartItem.deliveryOptionId;
-
-
-    html += `
-    <div class="delivery-option js-delivery-option"
-    data-product-id=${matchingProduct.id}
-    data-delivery-option-id=${deliveryOption.id}>
-    <input type="radio"  
-      ${isChecked ? 'checked' : ''} 
-      class="delivery-option-input"
-      name="delivery-option-${matchingProduct.id}">
-    <div>
-      <div class="delivery-option-date">
-        ${dateString}
+    cartSunmaryHTML +=
+    `
+    <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date: ${dateString}
       </div>
-      <div class="delivery-option-price">
-        ${priceString} Shipping
+
+      <div class="cart-item-details-grid">
+        <img class="product-image" src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
+        </div>
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          ${deliveryOptionsHTML(matchingProduct,cartItem)}
+        </div>
       </div>
     </div>
-  </div>
-  `
+    `
   })
-  return html;
 
+
+  function deliveryOptionsHTML(matchingProduct,cartItem){
+    let html = '';
+    deliveryOptions.forEach((deliveryOption)=>{
+      const today = dayjs();
+      const deliveryDate = today.add(
+        deliveryOption.deliveryDays, 
+        'days'
+      );
+      const dateString = deliveryDate.format(
+        'dddd, MMMM D'
+      );
+
+      const priceString = deliveryOption.priceCents 
+      === 0 
+      ? 'Free'
+      :  `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+      const isChecked = deliveryOption.id === 
+      cartItem.deliveryOptionId;
+
+
+      html += `
+      <div class="delivery-option js-delivery-option"
+      data-product-id=${matchingProduct.id}
+      data-delivery-option-id=${deliveryOption.id}>
+      <input type="radio"  
+        ${isChecked ? 'checked' : ''} 
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
+      <div>
+        <div class="delivery-option-date">
+          ${dateString}
+        </div>
+        <div class="delivery-option-price">
+          ${priceString} Shipping
+        </div>
+      </div>
+    </div>
+    `
+    })
+    return html;
+
+  }
+
+  document.querySelector('.order-summary')
+  .innerHTML = cartSunmaryHTML;
+
+
+  document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', ()=>{
+      const productId = link.dataset.productId;//需要知道点击的哪个产品：通过在button上面加产品id来获取id
+      removeFromCart(productId);
+      
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+        );
+      container.remove();
+    })
+  })
+
+  document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+    element.addEventListener('click',()=>{
+      const {productId, deliveryOptionId} = element.dataset; //从data attribute去获取
+      updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary() //调用自身函数，regenerate整个pages
+    })
+  })
 }
 
-document.querySelector('.order-summary')
-.innerHTML = cartSunmaryHTML;
-
-
-document.querySelectorAll('.js-delete-link')
-.forEach((link) => {
-  link.addEventListener('click', ()=>{
-    const productId = link.dataset.productId;//需要知道点击的哪个产品：通过在button上面加产品id来获取id
-    removeFromCart(productId);
-    
-
-    const container = document.querySelector(
-      `.js-cart-item-container-${productId}`
-      );
-    container.remove();
-  })
-})
-
-document.querySelectorAll('.js-delivery-option')
-.forEach((element)=>{
-  element.addEventListener('click',()=>{
-    const {productId, deliveryOptionId} = element.dataset; //从data attribute去获取
-    updateDeliveryOption(productId, deliveryOptionId);
-  })
-})
+renderOrderSummary() //调用函数
